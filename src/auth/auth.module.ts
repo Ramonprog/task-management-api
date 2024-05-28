38df70d7d65b5ddfe823ee4bb910f117b84a0c30
Ramonprog <programador.ramon@gmail.com>
2024-05-28 19:57:56 -0300

    core: configurando modulo de autenticação

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { Module } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { AuthController } from './auth.controller'
+import { JwtModule } from '@nestjs/jwt'
+import { ConfigService } from '@nestjs/config'
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: +configService.get<string | number>('JWT_EXPIRES_TIME'),
+        },
+      }),
+    }),
+  ],
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
 })
 export class AuthModule {}
